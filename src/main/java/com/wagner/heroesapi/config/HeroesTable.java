77dcde79cef997830652e8602a455baf81ba656b
wagnerkaba <bom.link+github@gmail.com>
2021-11-03 20:01:33 -0300

    Primeiro commit

---
+++ src/main/java/com/wagner/heroesapi/config/HeroesTable.java
@@ -0,0 +1,47 @@
+package com.wagner.heroesapi.config;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.*;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+import static com.wagner.heroesapi.constants.HeroesConstant.ENDPOINT_DYNAMO;
+import static com.wagner.heroesapi.constants.HeroesConstant.REGION_DYNAMO;
+
+@Configuration
+@EnableDynamoDBRepositories
+public class HeroesTable {
+
+
+    public static void main(String[] args) throws Exception{
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(ENDPOINT_DYNAMO, REGION_DYNAMO))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        String tableName="Heroes_Table";
+
+        try{
+            Table table = dynamoDB.createTable(tableName,
+                    Arrays.asList(new KeySchemaElement("id", KeyType.HASH)),
+                    Arrays.asList(new AttributeDefinition("id", ScalarAttributeType.S)),
+                    new ProvisionedThroughput(5L, 5L));
+
+            table.waitForActive();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+
+}
---
+++ src/test/java/com/wagner/heroesapi/HeroesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagner.heroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
