bf64cea6c273b6d86dbda2d0b5124284808ce4a3
wagnerkaba <bom.link+github@gmail.com>
2021-12-02 14:51:44 -0300

    Configuração do Swagger

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-#HEROES API
+# HEROES API
 
+Gerenciador de super heróis da Marvel e da DC em uma API reativa com Spring Boot
 
-Tecnologias utilizadas:
+Documentação disponível em: https://wkheroesapi.herokuapp.com//swagger-ui.html
+
+**Tecnologias utilizadas:**
+
 - Java 8
 - DynamoDB
 - Swagger
+- Junit
+- Spring WebFlux
+- Spring Boot
+
--- src/main/java/com/wagner/heroesapi/HeroesapiApplication.java
+++ src/main/java/com/wagner/heroesapi/HeroesapiApplication.java
@@ -1,10 +1,20 @@
 package com.wagner.heroesapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+// A anotação abaixo serve para configurar a documentação do Swagger
+@OpenAPIDefinition(info = @Info(
+		title = "HERO API",
+		version = "1.0",
+		description = "Gerenciador de super heróis da Marvel e da DC em uma API reativa com Spring Boot",
+		contact = @Contact(name = "Wagner Kaba", url = "https://github.com/wagnerkaba", email = "elguapore@gmail.com")))
 @EnableDynamoDBRepositories
 public class HeroesapiApplication {
 
